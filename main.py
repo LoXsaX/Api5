72904295caceee8cf5bdc7946ebd6d36aacf9d85
LoXsaX <119329239+LoXsaX@users.noreply.github.com>
2024-04-03 16:09:59 +0300

    Изменил название секрета

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def get_table_vacancy(statistic):
 
 if __name__ == '__main__':
     load_dotenv()
-    sj_token = os.environ['SECRET_KEY_SJ']
+    SuperJob_token = os.environ['SECRET_KEY_SJ']
     print(get_table_vacancy(get_statistic_sj(sj_token)))
     print(get_table_vacancy(get_statistic_hh())) 
 
